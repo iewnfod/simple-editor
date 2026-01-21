1ca246b425d0a79ce9a022fa2fa3c2dd1fc38945
Iewnfod <iewnfoddd@outlook.com>
2026-01-21 13:51:10 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import {SimpleEditor} from "@/components/tiptap-templates/simple/simple-editor.tsx";
+
+function App() {
+  return (
+    <SimpleEditor/>
+  );
+}
+
+export default App;
